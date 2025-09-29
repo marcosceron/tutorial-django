2e9e66a8c6e4e96a56b7a55988d12ae20a712b9f
Marcos <marcos.ceron89@gmail.com>
2025-09-29 16:41:25 -0300

    Adiciona models Pergunta e Alternativa

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,24 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Pergunta(models.Model):
+    texto_pergunta = models.CharField(max_length=200)
+    data_publicacao = models.DateTimeField("data de publicação")
+
+    def foi_publicado_recentemente(self):
+        return self.data_publicacao >= timezone.now() - datetime.timedelta(days=1)
+
+    def __str__(self):
+        return self.texto_pergunta
+    
+    
+
+class Alternativa(models.Model):
+    pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
+    texto_alternativa = models.CharField(max_length=200)
+    votos = models.IntegerField(default=0) 
+
+    def __str__(self):
+        return self.texto_alternativa
